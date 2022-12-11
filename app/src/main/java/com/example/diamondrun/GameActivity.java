fe8181e3276dcf7cd9363f146999eb2297adb1cd
Chris Feller <cfeller5547@sdsu.edu>
2022-12-10 17:51:30 -0800

    Merge branch 'master' of https://github.com/mattood/DiamondRun-HyperCasual-2D
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	build.gradle

--- app/src/main/java/com/example/diamondrun/GameActivity.java
+++ app/src/main/java/com/example/diamondrun/GameActivity.java
@@ -1,14 +1,26 @@
 package com.example.diamondrun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.Point;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
+
+    GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        gameView = new GameView(this, size.x, size.y);
+        setContentView(gameView);
+
     }
 }
--- app/src/main/java/com/example/diamondrun/GameView.java
+++ app/src/main/java/com/example/diamondrun/GameView.java
@@ -1,5 +1,30 @@
 package com.example.diamondrun;
 
-public class GameView {
+import android.content.Context;
+import android.view.View;
+import android.os.Handler;
+
+public class GameView extends View {
 // move the playerdiamond object
+
+    private Handler handler;
+    private Runnable r;
+
+    public GameView(Context context, int screenX, int screenY) {
+        super(context);
+
+        handler = new Handler();
+        r = new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+
+            }
+        };
+    }
+
+
+
+
+
 }
