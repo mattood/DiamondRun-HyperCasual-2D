3779339f7ecfed6288b93513e27bee4507b5a408
mattood <matthewsimon4@gmail.com>
2022-12-10 20:14:25 -0800

    Added .xml code for play button to load and be placed accordingly. play button then called from MainActivity.java, and works there

--- app/src/main/java/com/example/diamondrun/MainActivity.java
+++ app/src/main/java/com/example/diamondrun/MainActivity.java
@@ -17,41 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a new LinearLayout to hold the play button
-        LinearLayout layout = new LinearLayout(this);
-
-        // Set the orientation of the LinearLayout to vertical
-        layout.setOrientation(LinearLayout.VERTICAL);
-
-        // Create a new Button object
-        Button playButton = new Button(this);
-
-        // Set the text of the Button to "Play"
-        playButton.setText("Play");
-
-        // Set the background color of the Button to blue
-        playButton.setBackgroundColor(Color.BLUE);
 
 
-
-        final Activity activity = this;
-
-// Set the OnClickListener of the Button to open the next scene when clicked
-        playButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent object to open the GameActivity, using the activity variable instead of the this keyword
-                Intent intent = new Intent(activity, GameActivity.class);
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
 
-                // Start the GameActivity using the Intent
-                startActivity(intent);
             }
         });
 
-        // Add the Button to the LinearLayout
-        layout.addView(playButton);
-
-        // Set the layout of the Activity to the LinearLayout
-        setContentView(layout);
+      
     }
 }
