9fa0c98c9c28d297ab64ef1965128bb6ea8dcbdd
Chris Feller <cfeller5547@sdsu.edu>
2022-12-13 17:34:32 -0800

    update

--- app/src/main/java/com/example/diamondrun/DiamondCollection.java
+++ app/src/main/java/com/example/diamondrun/DiamondCollection.java
@@ -14,17 +14,20 @@ public class DiamondCollection {
     private Bitmap bitmap;
     private int topOfScreen=0;
     private int bitmapWidth = 0;
-    private int bitmapHeight = 100;
+    private int bitmapHeight = 150;
+    private int collectionYLocation;
 
     DiamondCollection(Context context, int numDiamonds, int screenWidth){
         bitmapWidth = screenWidth/numDiamonds;
         gameGrid = new GameGrid(numDiamonds, screenWidth);
         diamonds = new LinkedList<Diamond>();//initializing DiamondCollection
+
         spawnDiamonds(context, numDiamonds);
         for(int i = 0; i < diamonds.size(); i++){
             bitmap = diamonds.get(i).getRandomDiamondColorBitmap();
         }
     }
+
 
     public Diamond get_at(int index)
     {
@@ -43,9 +46,23 @@ public class DiamondCollection {
         return this.gameGrid;
     }
 
+    public int getYLocation(int index){
+        for(int i = 0; i < diamonds.size(); i++){ //going through entire linked list
+            collectionYLocation = diamonds.get(i).getYLocation();
+        }
+        return collectionYLocation;
+    }
+
+    public void resetYLocation(){
+       for(int i = 0; i < diamonds.size(); i++){
+           diamonds.get(i).setYLocation(topOfScreen-bitmapHeight);
+       }
+    }
+
     public int getXLocation(int index){
         return get_at(index).getXLocation();
     }
+
 
     public void spawnDiamonds(Context context, int numDiamonds){
         for(int i = 0; i < numDiamonds; i++){ //we setting the size of entire collection here where they are created
--- app/src/main/java/com/example/diamondrun/GameView.java
+++ app/src/main/java/com/example/diamondrun/GameView.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.View;
 import android.os.Handler;
+import android.widget.Toast;
 
 public class GameView extends View {
 
@@ -17,6 +18,9 @@ public class GameView extends View {
     private int numDiamonds = 5;
     private int screenX, screenY;
     private GameGrid gameGrid;
+    private boolean initActionDown = false;
+    int downX;
+    int upX;
 
     public GameView(Context context, int screenWidth, int screenHeight)  {
         super(context);
@@ -54,51 +58,81 @@ public class GameView extends View {
 
     public void update(){
         diamondCollection.moveDiamondsDownScreen();
+        if(diamondCollectionMovedBelowScreen()){ //continue respawning diamonds
+            diamondCollection.resetYLocation();
+            diamondCollection.moveDiamondsDownScreen();
+        }
     }
-
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         gameGrid = diamondCollection.getGameGrid();
 
-        switch(event.getAction()) {
+        switch (event.getAction()) {
 
-
-
-            case MotionEvent.ACTION_DOWN: //this is for taps, check this either first or second
-
-            if (gameGrid.insideAxis(0, (int) event.getX())) { //if he clicked inside axis 1
-                movePlayerDiamondIntoCenterAxis(0); //move him to same location
-                break;
-            }
-            if (gameGrid.insideAxis(1, (int) event.getX())) { //if he clicked inside axis 2
-                movePlayerDiamondIntoCenterAxis(1); //move him to same location
-                break;
-            }
-            if (gameGrid.insideAxis(2, (int) event.getX())) { //if he clicked inside axis 3
-                movePlayerDiamondIntoCenterAxis(2); //move him to same location
-                break;
-            }
-            if (gameGrid.insideAxis(3, (int) event.getX())) { //if he clicked inside axis 4
-                movePlayerDiamondIntoCenterAxis(3); //move him to same location
-                break;
-            }
-            if (gameGrid.insideAxis(4, (int) event.getX())) { //if he clicked inside axis 5
-                movePlayerDiamondIntoCenterAxis(4); //move him to same location
-                break;
-            }
+            case MotionEvent.ACTION_DOWN:
+                if(initActionDown == false) {
+                    downX = (int) event.getX();
+                    initActionDown = true;
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                upX = (int) event.getX();
+                if(motionIsSwipe(upX ,downX)) { //movement into axis only happens on swipe so first check if swipe occurred
+                    if (gameGrid.insideAxis(0, (int) event.getX())) { //if he clicked inside axis 1
+                        movePlayerDiamondIntoCenterAxis(0); //move him to same location
+                        break;
+                    }
+                    if (gameGrid.insideAxis(1, (int) event.getX())) { //if he clicked inside axis 2
+                        movePlayerDiamondIntoCenterAxis(1); //move him to same location
+                        break;
+                    }
+                    if (gameGrid.insideAxis(2, (int) event.getX())) { //if he clicked inside axis 3
+                        movePlayerDiamondIntoCenterAxis(2); //move him to same location
+                        break;
+                    }
+                    if (gameGrid.insideAxis(3, (int) event.getX())) { //if he clicked inside axis 4
+                        movePlayerDiamondIntoCenterAxis(3); //move him to same location
+                        break;
+                    }
+                    if (gameGrid.insideAxis(4, (int) event.getX())) { //if he clicked inside axis 5
+                        movePlayerDiamondIntoCenterAxis(4); //move him to same location
+                        break;
+                    }
+                }
         }
         return true;
+    }
+
+    private boolean diamondCollectionMovedBelowScreen(){
+        for(int i=0; i < numDiamonds; i++){ //axis length is number of diamonds
+            if(diamondCollection.getYLocation(1) > screenY){ //all index have same y location so using 1
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
     }
 
     public void movePlayerDiamondIntoCenterAxis(int i){
         playerDiamond.setXLocation(diamondCollection.getXLocation(i)); //set him to same x location as specific diamondCollection index
     }
 
+    public int distance(int var1, int var2){
+        return Math.abs(var2 - var1);
+    }
 
-
-
-
+    public boolean motionIsSwipe(int x1, int x2) {
+        if(distance(x1,x2) > 100){
+            return true;
+        }
+        else{
+            return false;
+        }
 
     }
+
+}
